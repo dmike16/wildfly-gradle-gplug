937f3735da7150806fd1fdb0264f931729422545
dmike16 <cipmiky@gmail.com>
2018-03-18 00:14:21 +0100

    [ADD] +first spec
    Created first spec.
    
    ==============================
    
     dmike16 <cipmiky@gmail.com>
    
    ==============================

---
+++ src/test/groovy/com/github/dmike16/cplug/spec/ClientCommandSpec.groovy
@@ -0,0 +1,13 @@
+package com.github.dmike16.cplug.spec;
+
+import spock.lang.Specification;
+import spock.lang.Ignore;
+/**
+* Specification to test Jboss Command used by the plugin 
+*/
+class ClientCommandSpec extends Specification {
+    @Ignore
+    def "deploy command to wildfly"(){
+    }
+}
+    
