4cd799b539912c0c2629ce24b02fec87566d9db5
dmike16 <cipmiky@gmail.com>
2018-05-05 23:21:39 +0200

    [MOD]  switch to idea CU editor
    ==============================
    
     dmike16 <cipmiky@gmail.com>
    
    ==============================

--- src/main/java/com/github/dmike/cplug/WildflyPlugin.java
+++ src/main/java/com/github/dmike/cplug/WildflyPlugin.java
@@ -2,10 +2,11 @@ package com.github.dmike.cplug;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.internal.impldep.org.jetbrains.annotations.NotNull;
 
 public class WildflyPlugin implements Plugin<Project>{
 
-	@Override public void apply(Project arg0) {
+	@Override public void apply(@NotNull Project arg0) {
 		// TODO 
 	}
 }
